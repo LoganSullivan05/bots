fa5f04286bc392cd85b0e3995c0a023b73400275
log-log-log <37670711+log-log-log@users.noreply.github.com>
2021-12-13 21:25:58 -0500

    Update duolingo.js

--- duolingo.js
+++ duolingo.js
@@ -55,8 +55,14 @@ async function test(){
         check_btn.click();
     }else{
         in_field.value = "no clue";
-        check_btn.classList.remove("LhRk3 _3HhhB _2NolF _275sd _1ZefG _2orIw");
-        check_btn.classList.add("_3HhhB _2NolF _275sd _1ZefG _2orIw _39N6z _9C_ii");
+        const rem_classes = "LhRk3 _3HhhB _2NolF _275sd _1ZefG _2orIw".split(" ");
+        const add_classes = "_3HhhB _2NolF _275sd _1ZefG _2orIw _39N6z _9C_ii".split(" ");
+        for(let i=0;i<rem_classes.length;i++){
+            check_btn.classList.remove(rem_classes[i]);
+        }
+        for(let i=0;i<add_classes.length;i++){
+            check_btn.classList.add(add_classes[i]);
+        }
         await sleep(100);
         check_btn.click();
         await sleep(200);
