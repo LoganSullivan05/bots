a03b051e8c50d031c88adccc5dc4d2a89b4f5915
log-log-log <37670711+log-log-log@users.noreply.github.com>
2021-12-17 09:17:27 -0500

    Update duolingo_stories.js

--- duolingo_stories.js
+++ duolingo_stories.js
@@ -8,18 +8,17 @@ var stop = false;
 async function test(){
   if(stop){return}
   await sleep(500);
+    /*
   const continue_el = getElementByXpath("//*[text() = 'Continue']");
   if(continue_el!=null){
     continue_el.parentElement.click();
     test();return
   }
+  */
   const btns = document.getElementsByTagName("button");
-  for(let i=1;i<btns.length-2;i++){
-    for(let j=1;j<btns.length-2;j++){
-      btns[i].click();
-      btns[j].click();
-      await sleep(25)
-    }
+  for(let i=1;i<btns.length;i++){
+    btns[i].click();
+    await sleep(25)
   }
   test();
   return
