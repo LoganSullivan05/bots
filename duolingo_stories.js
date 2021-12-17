53bebb86ad8795f22edf52fd5a72c251c84711fc
log-log-log <37670711+log-log-log@users.noreply.github.com>
2021-12-17 09:46:53 -0500

    Update duolingo_stories.js

--- duolingo_stories.js
+++ duolingo_stories.js
@@ -6,16 +6,24 @@ function sleep(ms) {
 }
 var stop = false;
 async function test(){
-  if(stop){return}
-  await sleep(500);
-  const btns = document.getElementsByTagName("button");
-  for(let i=0;i<btns.length;i++){
-    btns[i].click();
-    await sleep(25)
-  }
-  const continue_el = getElementByXpath("//*[text() = 'Continue']");
-  if(continue_el!=null){continue_el.parentElement.click()}
-  test();
-  return
+    if(stop){return}
+    await sleep(500);
+    const btns = document.getElementsByTagName("button");
+    if(getElementByXpath("//*[text() = 'Select the matching pairs']")!=null){
+        for(let i=0;i<btns.length;i++){
+            for(let j=0;i<btns.length;j++){
+            btns[i].click();
+            btns[j].click();
+            await sleep(25)
+        }
+    }
+    for(let i=0;i<btns.length;i++){
+        btns[i].click();
+        await sleep(25)
+    }
+    const continue_el = getElementByXpath("//*[text() = 'Continue']");
+    if(continue_el!=null){continue_el.parentElement.click()}
+    test();
+    return
 }
 test();
