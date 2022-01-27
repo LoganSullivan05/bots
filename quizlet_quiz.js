b7aefbce5776ebfd82b2519ea1222de25f44b65d
log-log-log <37670711+log-log-log@users.noreply.github.com>
2022-01-27 11:09:18 -0500

    Update quizlet_quiz.js

--- quizlet_quiz.js
+++ quizlet_quiz.js
@@ -2,6 +2,8 @@ const URL = window.location.href;
 const answer_page = URL.split("test")[0];
 let l0_terms = {},l1_terms = {};
 function answer(){
+    alert("l0: ");
+    alert(l0_terms);
     //16 articles; 1 contains 5 questions (matching)
     const question_containers = document.getElementsByTagName("article");
     for(let i=0;i<question_containers.length;i++){
